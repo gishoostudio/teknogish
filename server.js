e0d0bb32a8fbb38c13b5afbb2774081a5fec0f88
gishoostudio <fakhridardab@gmail.com>
2025-07-24 18:02:16 +0330

    Update server.js

--- server.js
+++ server.js
@@ -22,3 +22,7 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log('Server running on port', PORT);
 });
+const path = require('path');
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
