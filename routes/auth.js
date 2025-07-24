c77d889f3a76d81f4cff2068719fead346bbf053
gishoostudio <fakhridardab@gmail.com>
2025-07-24 17:51:34 +0330

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../authController');
+const authController = require('../controllers/authController');
 
 // ثبت‌نام
 router.post('/register', authController.register);
